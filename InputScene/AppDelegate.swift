55c4e2fa7d17d77bda5ce8cec153a15dbbd5d054
shaung <shaun.geng@gmail.com>
2016-02-29 11:17:40 +0800

    Initial Commit

---
+++ InputScene/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  InputScene
+//
+//  Created by ショーン on 2/29/16.
+//  Copyright © 2016 drunkplanet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
